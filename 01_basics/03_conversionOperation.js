d1deeed4c802b1e2adf947446a03083e72850dd0
Tinku Maji <120413794+tinkumaji@users.noreply.github.com>
2023-08-18 15:09:48 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = false
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanValue = Boolean(isLoggedIn)
+// console.log(booleanValue)
+
+// 1 => true; 0 => false
+// "" => false
+// "Tinku" => true
+
+let someNumber = 10
+let stringValue = String(someNumber)
+console.log(stringValue)
+console.log(typeof stringValue)
